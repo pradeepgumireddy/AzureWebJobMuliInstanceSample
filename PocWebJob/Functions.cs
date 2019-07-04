c7e4370588eadb8f47a2da1a955b72ce8b4e5598
Pradeep Kumar Reddy Gumireddy <Pradeep.Gumireddy@evry.com>
2019-07-04 12:46:45 +0530

    Created new azure webjob project for verifying the multiinstance

---
+++ PocWebJob/Functions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PocWebJob
+{
+    public class Functions
+    {
+        private TelemetryClient _tClient;
+        //[NoAutomaticTriggerAttribute]
+        public Functions(IConfiguration configuration)
+        {
+            _tClient = new TelemetryClient();
+            _tClient.Context.Operation.Id = Guid.NewGuid().ToString();
+            _tClient.Context.Operation.Name = "PocWebJob";
+        }
+        public async Task ProcessMessageFromQueue([TimerTrigger("0/01 * * * * *")]TimerInfo timerInfo)//TextWriter log
+        {
+            while (true)
+            {
+                string instanceid = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID");
+                Console.WriteLine($"InstanceID:{ instanceid}");
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine($"Count:{ i}");
+                }
+                _tClient.TrackTrace($"InstanceID:{ instanceid}");
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ PocWebJob/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PocWebJob
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddTimers();
+            });
+            builder.ConfigureServices(s =>
+            {
+                s.AddTransient<Functions, Functions>();
+                //s.Configure<ServiceBusSettings>(Configuration.GetSection("ServiceBusSettings"));
+                //s.AddApplicationInsightsTelemetry(Configuration);
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+                //If the key exists in settings, use it to enable Application Insights.
+                string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                if (!string.IsNullOrEmpty(instrumentationKey))
+                {
+                    b.AddApplicationInsights(o => o.InstrumentationKey = instrumentationKey);
+                }
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+        private static IConfiguration Configuration { get; set; }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            //var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                //.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddSingleton(Configuration);
+            //services.AddOptions();
+        }
+    }
+}
